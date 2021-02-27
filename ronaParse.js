a9d7d0c994a007cb4087bddb40ad785ddb9a5817
rnafng <35820548+rnafng@users.noreply.github.com>
2021-02-26 21:17:17 -0500

    Add files via upload

--- ronaParse.js
+++ ronaParse.js
@@ -1,28 +1,40 @@
+// Rona Fang, Feb 26 2021
+
+// ronaParse.js uses regex to find and display data of choice
+// through, given the path of the text file. 
+
 const fs = require('fs');
 const rls = require('readline-sync');
 
 var filePath = ""; // one may overwrite file path here
 
 if (0 === filePath.length){ // asks for file path if none given
-filePath = rls.question("enter file path\n"); 
+	filePath = rls.question("enter file path\n"); 
 }
 
 /**
 * Parses formatted bill text file and prints relevant data
-* @param {String} fp 	file path to the text file of formatte bill
-* @return {void} 
+* @param 	{String} 	fp 		file path to the text file
+* @return 	{void} 
 */
+
 function parse(fp){
 	var text = fs.readFileSync(fp, "utf8");
 	var custAccReg = /\d+ - \d+/;
 	console.log("1. Customer and Acc #: " + custAccReg.exec(text)[0]);
+	
 	// to access each value individually, could further /\d+/
 	var billPReg = /[A-Z][a-z]+ \d+, \d+ to [A-Z][a-z]+ \d+, \d+/;
 	console.log("2. Bill Period: " + billPReg.exec(text)[0]);
+
+	// would extract bill number including "Bill number: "
 	var billNReg = /Bill number: \d+/;
+	
+	// remove the "Bill number: " via /\d+/
 	console.log("3. Bill #: " + /\d+/.exec(billNReg.exec(text)[0])[0]); 
+
+	// takes the formatted string of the total charge, with a focus on "$" to ".", and the cent digits
 	var newChargeReg = /Total new charges\s+\$[^.]+.\d\d/;
 	console.log ("4. Total New Charges: " + /\$[^.]+.\d\d/.exec(newChargeReg.exec(text))[0]);
 }
-
 parse(filePath);
--- ronaScrape.js
+++ ronaScrape.js
@@ -1,10 +1,17 @@
+// Rona Fang, Feb 26 2021
+
+// ronaSrape.js uses puppeteer to navigate through condoworks.co
+// to search and download an invoice 
+
+
 const puppeteer = require('puppeteer');
 const path = require('path');
-
 (async () => {
- 	 const browser = await puppeteer.launch({headless:false, slowMo:75, defaultViewport:null});
+	
+ 	const browser = await puppeteer.launch({headless:false, slowMo:75, defaultViewport:null});
   	const page = await browser.newPage();
   	const dPath = path.resolve(__dirname, "./rona");
+  	await page.setDefaultNavigationTimeout(0); // no timeout limit
   	await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: dPath})
   	await page.goto('https://app-dev.condoworks.co/');
 	await page.type('#Email', 'coop.test@condoworks.co');
@@ -19,20 +26,28 @@ const path = require('path');
 	await page.waitForSelector('[name="invoices.InvoiceNumber"]');
 	await page.type('[name="invoices.InvoiceNumber"]', '123');
 	await page.waitForSelector('tr[class="jqgrow ui-row-ltr"]');
+	await page.waitForSelector('[title="123444"]');
+
   	await page.evaluate(() => {
-  		var rows = document.querySelectorAll('tr[class="jqgrow ui-row-ltr"]');
-  	var i=0;
-  
-  	for(i=0; i<rows.length; ++i){
-  		if (rows[i].querySelectorAll('[title="123444"]').length >0){
-  			rows[i].querySelector('button').setAttribute("hireMePls","true");
+  	var rows = document.querySelectorAll('tr[class="jqgrow ui-row-ltr"]'); // rows of invoices
+	  	var i=0;
+	  	for(i=0; i<rows.length; ++i){
+	  		if (rows[i].querySelectorAll('[title="123444"]').length >0){ 
+	  			// if the row has Invoice#123444
+	  			rows[i].querySelector('button').setAttribute("pls","hireMe"); 
+	  			// set an attribute to refer to outside of eval()
+	  		}	
   		}
-  	}});
-  	await page.waitForSelector('[hireMePls="true"]');
-  	await page.click('[hireMePls="true"]');
+  	});
+
+  	await page.waitForSelector('[pls="hireMe"]');
+  	await page.click('[pls="hireMe"]');
 	await page.waitForSelector('[title="Download file"]');
 	await page.click('[title="Download file"]');
+
+
+	console.log("Downloaded @ \n");
 	console.log(dPath + "\\Invoice file.pdf");
-	await page.waitForNavigation();
-	await browser.close();
+	
 })();
+ 
